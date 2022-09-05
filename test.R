9ae65dc76c645e2984ca3d7f8b3168ad13bdee89
cphstud <cphstud@gmail.com>
2022-09-05 23:02:20 +0200

    added

--- test.R
+++ test.R
@@ -1,1 +1,12 @@
 library(benchmarkme)
+
+df = data.frame(matrix(ncol = 8, nrow=0))
+cn = c("u")
+
+res = benchmark_matrix_cal()
+
+mres=get_cpu()
+vcore=mres[[3]]
+vmod=mres[[1]]
+vghz=unlist(strsplit(mres[[2]]," "))
+vghz <- vghz[[3]]
